4dc3d5176e6e988271d67636ddd8628af1ed9d42
Lab-ERP-17\Lab-ERP17 <indiradhekawanti@gmail.com>
2022-10-29 15:03:03 +0700

    First Commit

---
+++ app/src/main/java/com/chirikualii/materi_recyclerview/DataDummy.kt
@@ -0,0 +1,73 @@
+package com.chirikualii.materi_recyclerview
+
+object DataDummy  {
+
+    // TODO add data dummy
+    val listProfile = listOf<Profile>(
+        Profile(
+            name = "Indira",
+            phone = "081297555666",
+            image = "https://play-lh.googleusercontent.com/ulMAlLTgPLQwx9gCsQgCp2_YUrOep6Nx_s6BWxQhqwxCPMtVQ9EO37vrw6v8qtwtN3c"
+        ),
+
+        Profile(
+            name = "Jaehyun",
+            phone = "081297787878",
+            image = "https://awsimages.detik.net.id/community/media/visual/2021/01/22/jaehyun-nct_34.png?w=1200"
+        ),
+
+        Profile(
+            name = "Anisa",
+            phone = "081297434346",
+            image = "https://asianwiki.com/images/2/23/Joy_Red_Velvet-p2.jpg"
+        ),
+
+        Profile(
+            name = "Putri",
+            phone = "081297675765",
+            image = "https://avatars.githubusercontent.com/u/14894746?v=4"
+        ),
+
+        Profile(
+            name = "Dewi",
+            phone = "081297889889",
+            image = "https://cdn0-production-images-kly.akamaized.net/tI4P1Gim-5rjrEofIPtH77MVG4Q=/1200x900/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3611321/original/014876500_1635061240-monkey-4788331_1280.jpg"
+        ),
+
+        Profile(
+            name = "Ogi",
+            phone = "081297121212",
+            image = "https://cdn0-production-images-kly.akamaized.net/tI4P1Gim-5rjrEofIPtH77MVG4Q=/1200x900/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3611321/original/014876500_1635061240-monkey-4788331_1280.jpg"
+        ),
+
+        Profile(
+            name = "Indi",
+            phone = "081297121212",
+            image = "https://avatars.githubusercontent.com/u/14894746?v=4"
+        ),
+
+        Profile(
+            name = "Indi",
+            phone = "081297121212",
+            image = "https://avatars.githubusercontent.com/u/14894746?v=4"
+        ),
+
+        Profile(
+            name = "Indi",
+            phone = "081297121212",
+            image = "https://avatars.githubusercontent.com/u/14894746?v=4"
+        ),
+
+        Profile(
+            name = "Indi",
+            phone = "081297121212",
+            image = "https://avatars.githubusercontent.com/u/14894746?v=4"
+        ),
+
+        Profile(
+            name = "Indi",
+            phone = "081297121212",
+            image = "https://avatars.githubusercontent.com/u/14894746?v=4"
+        ),
+    )
+}
---
+++ app/src/main/java/com/chirikualii/materi_recyclerview/Profile.kt
@@ -0,0 +1,9 @@
+package com.chirikualii.materi_recyclerview
+
+import android.provider.ContactsContract.CommonDataKinds.Phone
+
+data class Profile (
+    val name:String,
+    val phone:String,
+    val image:String
+        )
